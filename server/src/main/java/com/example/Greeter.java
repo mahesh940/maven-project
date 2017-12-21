4b127e829591883cef355abc2c7142c20000402f
mahesh940 <mahesh.shimpi@lntinfotech.com>
2017-12-21 14:25:16 +0530

    add new comment

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,8 +11,9 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
+/**
+ * TODO: Add javadoc comment
+ */  
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
